a837d6614a275563eb28601a1ca4d799cec1fc88
Lars Kellogg-Stedman <lars@redhat.com>
2016-04-15 15:30:59 -0400

    moar scripts

--- azurify.sh
+++ azurify.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
+while getopts p: ch; do
+	case $ch in
+	(p) OPT_PASSWORD="$OPTARG";;
+	esac
+done
+shift $(( $OPTIND - 1 ))
+
 virt-customize -a "$1" \
+	${OPT_PASSWORD:+--root-password password:"$OPT_PASSWORD"} \
 	--install epel-release \
 	--install WALinuxAgent \
-	--upload cloud-init-0.7.7-2gita12ffde.el7.centos.x86_64.rpm:/tmp/cloud-init.rpm \
+	--upload cloud-init.rpm:/tmp/cloud-init.rpm \
 	--run-command "yum -y install /tmp/cloud-init.rpm" \
 	--upload cloud.cfg.d/azure.cfg:/etc/cloud/cloud.cfg.d/azure.cfg \
 	--upload waagent.conf:/etc/waagent.conf \
+	--upload waagent.service:/etc/systemd/system/waagent.service \
 	--run-command "systemctl enable waagent"
