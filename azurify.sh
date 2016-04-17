176ecb517e3ce8a14b207e8a64cc944d3029da46
Lars Kellogg-Stedman <lars@redhat.com>
2016-04-17 16:53:06 -0400

    waagent is not necessary

--- azurify.sh
+++ azurify.sh
@@ -12,16 +12,10 @@ echo "Adding Azure support to $1".
 virt-customize -a "$1" \
 	${OPT_PASSWORD:+--root-password password:"$OPT_PASSWORD"} \
 	--install epel-release \
-	--install WALinuxAgent \
 	--upload cloud-init.rpm:/tmp/cloud-init.rpm \
 	--run-command "yum -y install /tmp/cloud-init.rpm" \
 	--upload cloud.cfg.d/azure.cfg:/etc/cloud/cloud.cfg.d/azure.cfg \
-	--upload waagent.conf:/etc/waagent.conf \
-	--upload waagent.service:/etc/systemd/system/waagent.service \
 	--selinux-relabel
-
-# Install alternate version of the Azure data source
-#	--upload DataSourceAzure.py:/usr/lib/python2.7/site-packages/cloudinit/sources/DataSourceAzure.py \
 
 # Use this to disable selinux
 #	--run-command "sed -i '/^SELINUX=/ s/=.*/=permissive/' /etc/selinux/config" \
